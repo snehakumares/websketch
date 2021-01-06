2c0de0d1f067d49573515ce298db1c1aa1322d49
Sneha Kumaresan <53373541+snehakumares@users.noreply.github.com>
2021-01-06 23:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function openPage(pageName, elmnt, name) {
         show = document.getElementById("show");
         show.innerHTML = name;
     }
-    scrollToTop();
 }
 
 // Get the element with id="defaultOpen" and click on it
@@ -44,8 +43,28 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-/*
-function scrollToTop() {
-    window.scrollTo(0, 0);
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
-*/
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
