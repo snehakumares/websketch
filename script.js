f981b991247716530e8eed915c37b61a255d82ff
Sneha Kumaresan <53373541+snehakumares@users.noreply.github.com>
2021-01-08 01:40:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,6 @@ function openPage(pageName, elmnt, name) {
 
     // Show the specific tab content
     document.getElementById(pageName).style.display = "block";
-    if (pageName == "Contact" || pageName == "Category_1") {
-        document.getElementById(pageName).style.display = "flex";
-    }
     if (elmnt == "topnav") {
         var topnav = document.getElementById("myTopnav");
         var i;
@@ -67,4 +64,28 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+// Next/previous controls
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+    var i;
+    var slides = document.getElementsByClassName("clientSlides");
+    if (n > slides.length) { slideIndex1 = 1 }
+    if (n < 1) { slideIndex1 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex1 - 1].style.display = "flex";
 }
